be35272057840461a642dd994920310b7523d39d
Bartolyna <40046460+Bartolyna@users.noreply.github.com>
2021-07-28 00:17:26 -0500

    Se muestra el listado de usuarios registrados, enla parte del administrador

--- routes/admin.php
+++ routes/admin.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 
 
 // El prefijo que llevara antes, durante toda las rutas
 Route::prefix('/admin')->group( function () {
    
     Route::get('/', [DashboardController::class, 'getDashboard']);
-
+    Route::get('/users', [UserController::class, 'getUsers']);
 });
 
 ?>
